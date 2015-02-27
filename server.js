9fc1d3e69538c54ae666e995030c9605419cb8bf
Julia Jacobs <jjacobs2000@me.com>
2015-02-27 13:10:14 -0600

    updated express server

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-
 var express = require('express'),
-files = require('./routes/files'),
-path = require('path'),
-osprey = require('osprey'),
-fs = require('fs'),
-mock = require('./routes/mock'),
-_ = require('lodash'),
-routes = require('./routes/');
+    files = require('./routes/files'),
+    path = require('path'),
+    osprey = require('osprey'),
+    fs = require('fs'),
+    mock = require('./routes/mock'),
+    _ = require('lodash'),
+    routes = require('./routes/'),
+    morgan  = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    errorhandler = require('errorhandler'),
+    port = parseInt(process.env.PORT, 10) || 8081;
  
 var app = module.exports = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 8081);
-  app.use(express.logger('dev'));
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/api-designer'));
-  app.use(express.errorHandler({
+/* Configure a simple logger and an error handler. */
+app.use(morgan('combined'));
+app.use(errorhandler());
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + '/api-designer'));
+app.use(errorHandler({
     dumpExceptions: true,
     showStack: true
-  }));
-  app.use(app.router);
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+}));
 
 /**
  * ------
@@ -70,6 +68,6 @@ app.delete('/files/:id', files.deleteFile);
 app.get('/files/name/:name', files.findContentByName);
 app.get('/', routes.index);
 
-app.listen(app.get("port"));
+app.listen(port);
 
-console.log('Listening on port 8081...');
+console.log('Listening on port ' + port);
